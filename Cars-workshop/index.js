8bfaa4de45a2c6e838c0fb995a47d0a29f29cfaf
Aleks Nikolov <tcr.official.world@gmail.com>
2022-01-23 00:32:18 +0200

    app is very ready

--- Cars-workshop/index.js
+++ Cars-workshop/index.js
@@ -1,9 +1,43 @@
-const env = process.env.NODE_ENV || 'development';
+const express = require('express');
+const hbs = require('express-handlebars');
 
-const config = require('./config/config')[env];
-const app = require('express')();
+const carsService = require('./services/cars');
 
-require('./config/express')(app);
-require('./config/routes')(app);
+const {home} = require('./controllers/home');
+const {about} = require('./controllers/about');
+const create = require('./controllers/create');
+const {details} = require('./controllers/details');
+const edit = require('./controllers/edit');
+const deleteCar = require('./controllers/delete');
+const {notFound} = require('./controllers/404');
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+const app = express();
+
+app.engine('hbs', hbs.create({
+    extname: 'hbs',
+}).engine);
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({extended: true}));
+app.use('/static/', express.static('static'));
+app.use(carsService());
+
+app.get('/', home);
+app.get('/about', about);
+app.get('/details/:id', details);
+
+app.route('/create')
+    .get(create.get)
+    .post(create.post);
+
+app.route('/delete/:id')
+    .get(deleteCar.get)
+    .post(deleteCar.post);
+
+app.route('/edit/:id')
+    .get(edit.get)
+    .post(edit.post);
+
+app.all('*', notFound);
+
+app.listen(3000, () => console.log('Server started on port 3000\nNema zadna has mo karesh'));
