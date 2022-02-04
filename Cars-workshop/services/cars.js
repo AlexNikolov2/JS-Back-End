72faddbac97f3985ea91e06591c9c0d812474858
Aleks Nikolov <tcr.official.world@gmail.com>
2022-02-04 14:05:47 +0200

    merkava services... veri readi

--- Cars-workshop/services/cars.js
+++ Cars-workshop/services/cars.js
@@ -3,7 +3,9 @@ const { carViewModel } = require('./util');
 
 
 async function getAll(query) {
-    const options = {};
+    const options = {
+        isDeleted: false
+    };
 
     if (query.search) {
         options.name = new RegExp(query.search, 'i');
@@ -23,7 +25,7 @@ async function getAll(query) {
 }
 
 async function getById(id) {
-    const car = await Car.findById(id).populate('accessories');
+    const car = await Car.findById(id).where({ isDeleted: false }).populate('accessories');
     if (car) {
         return carViewModel(car);
     } else {
@@ -36,12 +38,25 @@ async function createCar(car) {
     await result.save();
 }
 
-async function deleteById(id) {
-    await Car.findByIdAndDelete(id);
+async function deleteById(id, ownerId) {
+    // await Car.findByIdAndDelete(id);
+    const existing = await Car.findById(id).where({ isDeleted: false });
+
+    if (existing.owner != ownerId) {
+        return false;
+    }
+
+    await Car.findByIdAndUpdate(id, { isDeleted: true });
+
+    return true;
 }
 
-async function updateById(id, car) {
-    const existing = await Car.findById(id);
+async function updateById(id, car, ownerId) {
+    const existing = await Car.findById(id).where({ isDeleted: false });
+
+    if (existing.owner != ownerId) {
+        return false;
+    }
 
     existing.name = car.name;
     existing.description = car.description;
@@ -50,10 +65,16 @@ async function updateById(id, car) {
     existing.accessories = car.accessories;
 
     await existing.save();
+
+    return true;
 }
 
-async function attachAccessory(carId, accessoryId) {
+async function attachAccessory(carId, accessoryId, ownerId) {
     const existing = await Car.findById(carId);
+
+    if (existing.owner != ownerId) {
+        return false;
+    }
 
     existing.accessories.push(accessoryId);
 
--- Cars-workshop/services/util.js
+++ Cars-workshop/services/util.js
@@ -1,10 +1,13 @@
+const bcrypt = require('bcrypt');
+
 function accessoryViewModel(accessory) {
     return {
         id: accessory._id,
         name: accessory.name,
         description: accessory.description,
         imageUrl: accessory.imageUrl,
-        price: accessory.price
+        price: accessory.price,
+        owner: accessory.owner
     };
 }
 
@@ -15,7 +18,8 @@ function carViewModel(car) {
         description: car.description,
         imageUrl: car.imageUrl,
         price: car.price,
-        accessories: car.accessories
+        accessories: car.accessories,
+        owner: car.owner
     };
 
     if (model.accessories.length > 0 && model.accessories[0].name) {
@@ -25,7 +29,29 @@ function carViewModel(car) {
     return model;
 }
 
+async function hashPassword(password) {
+    const salt = await bcrypt.genSalt(9);//sorry, but my PC is slow is f*ck
+    return await bcrypt.hash(password, salt);
+}
+
+async function comparePassword(password, hashedPassword) {
+    return await bcrypt.compare(password, hashedPassword);
+}
+
+function isLoggedIn() {
+    return (req, res, next) => {
+        if (req.session.user) {
+            next();
+        } else {
+            res.redirect('/login');
+        }
+    };
+}
+
 module.exports = {
     accessoryViewModel,
-    carViewModel
+    carViewModel,
+    hashPassword,
+    comparePassword,
+    isLoggedIn
 };
