64195d78896fc72a3d2cc7c344e3b7ab815a35df
Aleks Nikolov <72394391+AlexNikolov2@users.noreply.github.com>
2022-02-19 00:12:52 +0200

    Tomorrow weekend

---
+++ exam-preparation/shoe-shelf/middlewares/guards.js
@@ -0,0 +1,39 @@
+const offerService = require('../services/offer');
+
+const isAuth = () => {
+    return (req, res, next) => {
+        if (req.user !== undefined) {
+            next();
+        } else {
+            res.redirect('/auth/login');
+        }
+    };
+};
+
+const isGuest = () => {
+    return (req, res, next) => {
+        if (req.user == undefined) {
+            next();
+        } else {
+            res.redirect('/');
+        }
+    };
+};
+
+const isCreator = () => async (req, res, next) => {
+    const offer = await offerService.getById(req.params.id);
+    const isCreator = req.user && offer.creator == req.user._id;
+
+    if (!isCreator) {
+        res.redirect('/');
+        return;
+    } 
+    next();
+}
+
+
+module.exports = {
+    isAuth,
+    isGuest,
+    isCreator
+}
