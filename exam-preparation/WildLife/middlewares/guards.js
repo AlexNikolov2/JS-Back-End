78de9a5c5653f442f9b17f7e3a84eb73453b07ee
Aleks Nikolov <tcr.official.world@gmail.com>
2022-02-22 20:07:35 +0200

    more than you know, yeah, more than  you know

--- exam-preparation/WildLife/middlewares/guards.js
+++ exam-preparation/WildLife/middlewares/guards.js
@@ -1,5 +1,3 @@
-const postService = require('../services/post');
-
 const isAuth = () => {
     return (req, res, next) => {
         if (req.user !== undefined) {
@@ -20,20 +18,8 @@ const isGuest = () => {
     };
 };
 
-const isCreator = () => async (req, res, next) => {
-    const post = await postService.getOneById(req.params.id);
-    const isCreator = req.user && post.creator == req.user._id;
-
-    if (!isCreator) {
-        res.redirect('/');
-        return;
-    } 
-    next();
-};
-
 
 module.exports = {
     isAuth,
     isGuest,
-    isCreator
 };
