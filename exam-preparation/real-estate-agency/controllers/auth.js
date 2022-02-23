87d88b5bd4eeea0250feccc359b453d63274c5fd
Aleks Nikolov <tcr.official.world@gmail.com>
2022-02-23 12:36:21 +0200

    idk

--- exam-preparation/real-estate-agency/controllers/auth.js
+++ exam-preparation/real-estate-agency/controllers/auth.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const {isGuest, isAuth} = require('../middlewares/auth');
 const authService = require('../services/auth');
 
-const {cookie_name} = require('../config');
+const {cookie_name}=require('../config');
 
 router.get('/login', isGuest, (req, res)=>{
     res.render('auth/login');
@@ -29,7 +29,6 @@ router.get('/register', isGuest, (req, res)=>{
 
 router.post('/register', isGuest, async (req, res)=>{
     const{name, username, password, rePassword} = req.body;
-
     if(password !== rePassword){
         res.locals.error = 'Password mismatch';
         return res.render('auth/register');
@@ -46,8 +45,19 @@ router.post('/register', isGuest, async (req, res)=>{
 
         res.redirect('/');
 
-    }catch(err){
-        res.end();
+    }
+    catch (err) {
+        let errorMSG = '';
+
+        if (error.name === 'MongoError' && error.code === 11000) {
+            errorMSG = 'Username or email already exists!';
+        } else if (error.errors) {
+            errorMSG = Object.values(error.errors).map(x => x.properties.message)[0];
+        } else {
+            errorMSG = error.message;
+        }
+
+        res.render('register', { title: 'Register', error: errorMSG, oldData: { username, email } });
     }
 });
 
